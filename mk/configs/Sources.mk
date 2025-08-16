777cc4389bcfc6107372e12b3ac664ea5a39aa40
M-Jorda <j.jorda@gmail.com>
2025-08-16 16:25:26 +0200

    Makefile.mk

--- mk/configs/Sources.mk
+++ mk/configs/Sources.mk
@@ -1,16 +1,24 @@
-# ********************************* BONUS *********************************** #
+# ******************************** MANDATORY ********************************* #
 
 MAN			= philo/
-SRC			= $(MAN)main.c
 
-# ********************************* BONUS *********************************** #
+#						---------   SETUP	---------						   #
+SUP			= $(MAN)setup/
+SUP_SRC		= $(SUP)init.c $(SUP)mutexes.c $(SUP)philo.c
 
-BNS_DIR		= philo_bonus/
-BNS_C		= _bonus.c
-BNS_SRC		= $(BNS_DIR)main.c
+# 						---------   UTILS	---------						   #
+UTI_SRC		= $(MAN)utils.c
+
+# 						---------  CLEANUP  ---------						   #
+CUP			= $(MAN)cleanup/
+CUP_SRC		= $(CUP)cleanup.c
+
+# 						---------  SOURCES  ---------						   #
+MAN_SRC		= $(MAN)main.c $(SUP_SRC) $(UTI_SRC) $(CUP_SRC)
+
 
 # ********************************* OBJECTS ********************************** #
 
 DIR_OBJ		= obj
 DIR			= $(DIR_OBJ)/
-OBJ			= $(patsubst %.c, $(DIR)%.p, $(SRC))
+OBJ			= $(patsubst %.c, $(DIR)%.p, $(MAN_SRC))
---
+++ mk/configs/Sources_bonus.mk
@@ -0,0 +1,6 @@
+# ********************************** BONUS *********************************** #
+
+BNS_DIR		= philo_bonus/
+BNS_C		= _bonus.c
+BNS_SRC		= $(BNS_DIR)main.c
+
