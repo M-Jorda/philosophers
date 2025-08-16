9bbbb65b751627c9167093071612ea90d392f4ee
M-Jorda <j.jorda@gmail.com>
2025-08-16 16:30:47 +0200

    main.c

--- philo/main.c
+++ philo/main.c
@@ -6,17 +6,25 @@
 /*   By: jjorda <jjorda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 22:49:24 by jjorda            #+#    #+#             */
-/*   Updated: 2025/08/13 11:10:07 by jjorda           ###   ########.fr       */
+/*   Updated: 2025/08/16 16:04:59 by jjorda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/philo.h"
+
+static inline void	validate_args(int argc, char **argv)
+{
+	if (argc < 5)
+		printerr(NOT_ENOUGHT_ARGS, 1);
+	else if (argc > 6)
+		printerr(TOO_MUCH_ARGS, 1);
+}
 
 int	main(int argc, char **argv, char **env)
 {
 	t_data	data;
 
 	(void) env;
-	
-	init(&data, argc, &argv[1]);
+	validate_args(argc, argv);
+	init(&data, &argv[1]);
 }
